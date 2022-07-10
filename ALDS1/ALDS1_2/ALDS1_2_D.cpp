67c232e9e17169d5fcd93bd62cc9b080610db803
Ohkabe Fumiya <ookabe@43s.co.jp>
2022-07-11 06:39:08 +0900

    Shell Sort

--- ALDS1/ALDS1_2/ALDS1_2_D.cpp
+++ ALDS1/ALDS1_2/ALDS1_2_D.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-int cnt = 0;
+int cnt;
 
-int *insertionSort(int *A, int n, int g) {
+void insertionSort(int *A, int n, int g) {
   for (int i = g; i < n; i++) {
     int v = A[i];
     int j = i - g;
     while (j >= 0 && A[j] > v) {
         A[j+g] = A[j];
-        j = j - g;
+        j -= g;
         cnt++;
     }
     A[j+g] = v;
   }
-
-  return A;
 }
 
-int *shellSort(int *A, int n) {
-  vector<int> G;
+void shellSort(int *A, int n) {
+  cnt = 0;
   int r = 1;
+  vector<int> G;
+
   while (r <= n) {
     G.push_back(r);
     r *= 2;
   }
 
   int m = G.size();
-  reverse(G.begin(),G.end());
+  reverse(G.begin(), G.end());
 
   cout << m << endl;
-  for(int i = 0; i < m; i++) cout << G.at(i) << " \n" [i == m-1];
-
-  for(int i = 0; i < m; i++) A = insertionSort(A , n , G.at(i));
+  rep(i, m) cout << G[i] << " \n" [i == m-1];
+  rep(i, m) insertionSort(A , n , G[i]);
   cout << cnt << endl;
-
-  return A;
 }
 
 int main() {
   int n;
-  int A[n];
-
   cin >> n;
-  for (int i = 0; i < n; i++) cin >> A[i];
 
-  int Ans[n] = shellSort(A, n);
-  for(int k = 0; k < n; k++) cout << Ans[k] << endl;
+  int A[n];
+  rep(i, n) cin >> A[i];
+
+  shellSort(A, n);
+  rep(i, n) cout << A[i] << endl;
 
   return 0;
 }
